6500fc83cad2beb7441bb4c51aabbd15632ffce8
Andryss <andr.krivosheev@gmail.com>
2023-10-31 19:31:24 +0300

    Init spring boot project

---
+++ src/main/java/ru/itmo/trousseau/TrousseauApplication.java
@@ -0,0 +1,13 @@
+package ru.itmo.trousseau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrousseauApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrousseauApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/itmo/trousseau/TrousseauApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itmo.trousseau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrousseauApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
