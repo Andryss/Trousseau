e9d8238a0e6db94617c7a876a5f0032a0a46d504
Andryss <andr.krivosheev@gmail.com>
2023-11-15 23:16:43 +0300

    Add functions and procedures

---
+++ src/main/resources/sql/drop_functions.sql
@@ -0,0 +1,4 @@
+drop function if exists find_items, find_booked_items,
+    find_saved_items, find_subscriptions;
+
+drop procedure if exists book_item, close_item;
--- src/main/resources/sql/fill_data.sql
+++ src/main/resources/sql/fill_data.sql
@@ -1,91 +1,91 @@
-insert into dormitories values
-    (1, 'Студенческий городок', 'Санкт-Петербург, пер. Вяземский, д. 5/7'),
-    (2, 'Общежитие №2', 'Санкт-Петербург, ул. Ленсовета, д. 23, лит. А'),
-    (3, 'Общежитие №3', 'Санкт-Петербург, Альпийский пер., д. 15, к. 2, лит. А'),
-    (4, 'Общежитие №4', 'Санкт-Петербург, ул. Белорусская, д. 6, лит. А');
+insert into dormitories (name, address) values
+    ('Студенческий городок', 'Санкт-Петербург, пер. Вяземский, д. 5/7'),
+    ('Общежитие №2', 'Санкт-Петербург, ул. Ленсовета, д. 23, лит. А'),
+    ('Общежитие №3', 'Санкт-Петербург, Альпийский пер., д. 15, к. 2, лит. А'),
+    ('Общежитие №4', 'Санкт-Петербург, ул. Белорусская, д. 6, лит. А');
 
-insert into users values
-    (1, 'admin', 'password-hash', 'ADMIN', 1, '101'),
-    (2, 'user1', 'password-hash', 'USER', 1, '205'),
-    (3, 'user2', 'password-hash', 'USER', 1, '803'),
-    (4, 'user3', 'password-hash', 'USER', 1, '203');
+insert into users (login, password, role, dormitory_id, room) values
+    ('admin', 'password-hash', 'ADMIN', 1, '101'),
+    ('user1', 'password-hash', 'USER', 1, '205'),
+    ('user2', 'password-hash', 'USER', 1, '803'),
+    ('user3', 'password-hash', 'USER', 1, '203');
 
-insert into photos values
-    (1, pg_read_binary_file('/Users/andryssssss/Programming/Projects/Java/ITMO/Trousseau/src/main/resources/sql/img/1.jpg'), '2023-09-01 10:00', 2),
-    (2, pg_read_binary_file('/Users/andryssssss/Programming/Projects/Java/ITMO/Trousseau/src/main/resources/sql/img/2.jpg'), '2023-09-01 10:30', 2),
-    (3, pg_read_binary_file('/Users/andryssssss/Programming/Projects/Java/ITMO/Trousseau/src/main/resources/sql/img/3.jpg'), '2023-09-02 13:00', 2),
-    (4, pg_read_binary_file('/Users/andryssssss/Programming/Projects/Java/ITMO/Trousseau/src/main/resources/sql/img/4.jpg'), '2023-09-03 15:00', 3),
-    (5, pg_read_binary_file('/Users/andryssssss/Programming/Projects/Java/ITMO/Trousseau/src/main/resources/sql/img/5.jpg'), '2023-09-03 16:00', 3);
+insert into photos (data, upload_datetime, upload_user_id) values
+    (pg_read_binary_file('/Users/andryssssss/Programming/Projects/Java/ITMO/Trousseau/src/main/resources/sql/img/1.jpg'), '2023-09-01 10:00', 2),
+    (pg_read_binary_file('/Users/andryssssss/Programming/Projects/Java/ITMO/Trousseau/src/main/resources/sql/img/2.jpg'), '2023-09-01 10:30', 2),
+    (pg_read_binary_file('/Users/andryssssss/Programming/Projects/Java/ITMO/Trousseau/src/main/resources/sql/img/3.jpg'), '2023-09-02 13:00', 2),
+    (pg_read_binary_file('/Users/andryssssss/Programming/Projects/Java/ITMO/Trousseau/src/main/resources/sql/img/4.jpg'), '2023-09-03 15:00', 3),
+    (pg_read_binary_file('/Users/andryssssss/Programming/Projects/Java/ITMO/Trousseau/src/main/resources/sql/img/5.jpg'), '2023-09-03 16:00', 3);
 
-insert into category_groups values
-    (1, 'Мебель', '???'),
-    (2, 'Техника', '???'),
-    (3, 'Одежда', '???'),
-    (4, 'Посуда', '???'),
-    (5, 'Хозтовары', '???'),
-    (6, 'Другое', 'Все, что не подошло под другие группы категорий');
+insert into category_groups (name, description) values
+    ('Мебель', '???'),
+    ('Техника', '???'),
+    ('Одежда', '???'),
+    ('Посуда', '???'),
+    ('Хозтовары', '???'),
+    ('Другое', 'Все, что не подошло под другие группы категорий');
 
-insert into categories values
-    (1, 'Стул', '???', 1),
-    (2, 'Стол', '???', 1),
-    (3, 'Полка', '???', 1),
-    (4, 'Тумба', '???', 1),
-    (5, 'Провод', '???', 2),
-    (6, 'Зарядка', '???', 2),
-    (7, 'Внешний аккумулятор', '???', 2),
-    (8, 'Мышка', '???', 2),
-    (9, 'Внешний накопитель', '???', 2),
-    (10, 'Перчатки', '???', 3),
-    (11, 'Шапка', '???', 3),
-    (12, 'Нижнее белье', '???', 3),
-    (13, 'Носки', '???', 3),
-    (14, 'Тарелка', '???', 4),
-    (15, 'Вилка', '???', 4),
-    (16, 'Ложка', '???', 4),
-    (17, 'Кухонный нож', '???', 4),
-    (18, 'Тряпка', '???', 5),
-    (19, 'Гель для душа', '???', 5),
-    (20, 'Мыло', '???', 5),
-    (21, 'Зубная щетка', '???', 5),
-    (22, 'Другое', 'Все, что не подошло под другие категории', 6);
+insert into categories (name, description, group_id) values
+    ('Стул', '???', 1),
+    ('Стол', '???', 1),
+    ('Полка', '???', 1),
+    ('Тумба', '???', 1),
+    ('Провод', '???', 2),
+    ('Зарядка', '???', 2),
+    ('Внешний аккумулятор', '???', 2),
+    ('Мышка', '???', 2),
+    ('Внешний накопитель', '???', 2),
+    ('Перчатки', '???', 3),
+    ('Шапка', '???', 3),
+    ('Нижнее белье', '???', 3),
+    ('Носки', '???', 3),
+    ('Тарелка', '???', 4),
+    ('Вилка', '???', 4),
+    ('Ложка', '???', 4),
+    ('Кухонный нож', '???', 4),
+    ('Тряпка', '???', 5),
+    ('Гель для душа', '???', 5),
+    ('Мыло', '???', 5),
+    ('Зубная щетка', '???', 5),
+    ('Другое', 'Все, что не подошло под другие категории', 6);
 
-insert into items values
-    (1, 'Провод переходник USB/Type-c', 1, '????????????????????', 'ACTIVE', 2, '2023-09-01 10:20'),
-    (2, 'Стул ортопедический на колесиках', 2, '????????????????????', 'ACTIVE', 2, '2023-09-01 10:50'),
-    (3, 'Набор шампуней и гелей для душа', 3, '????????????????????', 'ACTIVE', 2, '2023-09-02 13:20'),
-    (4, 'Шапка ушанка', 4, '????????????????????', 'BLOCKED', 3, '2023-09-03 15:20'),
-    (5, 'Трусы с волком', 5, '????????????????????', 'ACTIVE', 3, '2023-09-03 16:20');
+insert into items (title, photo_id, description, status, user_id, creation_datetime) values
+    ('Провод переходник USB/Type-c', 1, '????????????????????', 'ACTIVE', 2, '2023-09-01 10:20'),
+    ('Стул ортопедический на колесиках', 2, '????????????????????', 'ACTIVE', 2, '2023-09-01 10:50'),
+    ('Набор шампуней и гелей для душа', 3, '????????????????????', 'BLOCKED', 2, '2023-09-02 13:20'),
+    ('Шапка ушанка', 4, '????????????????????', 'ACTIVE', 3, '2023-09-03 15:20'),
+    ('Трусы с волком', 5, '????????????????????', 'ACTIVE', 3, '2023-09-03 16:20');
 
-insert into item_status_history values
-    (1, 1, 'ARCHIVED', 'ACTIVE', '2023-09-01 10:21', 2),
-    (2, 2, 'ARCHIVED', 'ACTIVE', '2023-09-01 10:51', 2),
-    (3, 3, 'ARCHIVED', 'ACTIVE', '2023-09-02 13:21', 2),
-    (4, 4, 'ARCHIVED', 'ACTIVE', '2023-09-03 15:21', 3),
-    (5, 5, 'ARCHIVED', 'ACTIVE', '2023-09-03 16:21', 3),
-    (6, 3, 'ACTIVE', 'BLOCKED', '2023-09-03 18:00', 4);
+insert into item_status_history (item_id, old_status, new_status, change_datetime, change_user_id) values
+    (1, 'ARCHIVED', 'ACTIVE', '2023-09-01 10:21', 2),
+    (2, 'ARCHIVED', 'ACTIVE', '2023-09-01 10:51', 2),
+    (3, 'ARCHIVED', 'ACTIVE', '2023-09-02 13:21', 2),
+    (4, 'ARCHIVED', 'ACTIVE', '2023-09-03 15:21', 3),
+    (5, 'ARCHIVED', 'ACTIVE', '2023-09-03 16:21', 3),
+    (3, 'ACTIVE', 'BLOCKED', '2023-09-03 18:00', 4);
 
-insert into bookings values
-    (1, 4, 3, '2023-09-03 18:00');
+insert into bookings (user_id, item_id, booking_datetime) values
+    (4, 3, '2023-09-03 18:00');
 
-insert into saved_items values
+insert into saved_items (user_id, item_id, added_datetime) values
     (4, 3, '2023-09-03 15:00'),
     (4, 4, '2023-09-03 15:05'),
     (2, 3, '2023-09-03 13:25'),
     (4, 5, '2023-09-04 15:05');
 
-insert into subscriptions values
-    (1, 4, 'Хочу гель или мыло для душа', '2023-09-01 15:10'),
-    (2, 4, 'Ищу что бы надеть', '2023-09-01 15:30'),
-    (3, 3, 'Что-нибудь на замену волко-трусам', '2023-09-03 16:30');
+insert into subscriptions (user_id, name, created_datetime) values
+    (4, 'Хочу гель или мыло для душа', '2023-09-01 15:10'),
+    (4, 'Ищу что бы надеть', '2023-09-01 15:30'),
+    (3, 'Что-нибудь на замену волко-трусам', '2023-09-03 16:30');
 
-insert into item_categories values
+insert into item_categories (item_id, category_id) values
     (1, 5),
     (2, 1),
     (3, 19),
     (4, 11),
     (5, 12);
 
-insert into subscription_categories values
+insert into subscription_categories (subscription_id, category_id) values
     (1, 19),
     (1, 20),
     (2, 10),
