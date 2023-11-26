58a0440ea96f614b1f66ca4232a9520c5b106fb3
Andryss <andr.krivosheev@gmail.com>
2023-11-26 16:00:32 +0300

    Add hash indexes

--- src/main/resources/sql/create_index.sql
+++ src/main/resources/sql/create_index.sql
@@ -6,4 +6,8 @@ create index if not exists items_description_trgm on items using gin(description
 
 create index if not exists categories_name_trgm on categories using gin(name gin_trgm_ops);
 
--- TODO: добавить hash индексы на user_id
+create index if not exists bookings_user_id_hash on bookings using hash(user_id);
+
+create index if not exists saved_items_user_id_hash on saved_items using hash(user_id);
+
+create index if not exists subscriptions_user_id_hash on subscriptions using hash(user_id);
--- src/main/resources/sql/drop_index.sql
+++ src/main/resources/sql/drop_index.sql
@@ -1,1 +1,2 @@
-drop index if exists items_title_trgm, items_description_trgm, categories_name_trgm;
+drop index if exists items_title_trgm, items_description_trgm, categories_name_trgm,
+    bookings_user_id_hash, saved_items_user_id_hash, subscriptions_user_id_hash;
